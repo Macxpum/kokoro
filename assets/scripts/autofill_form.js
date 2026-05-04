bbcebbcc66077bef5731582dd4f11d85d597052a
Macxpum <mr_mashrum@Mail.ru>
2026-05-04 20:22:36 +0300

    Упрощение логики функций denis и igor, добавление проверки перед установкой атрибутов

--- assets/scripts/autofill_form.js
+++ assets/scripts/autofill_form.js
@@ -1,48 +1,56 @@
 function denis() {
   window.location.href = "subscribe.php";
-  console.log("Denis");
-  const childCheckBox = document.querySelector("input[name='group-child']");
-  const teenageCheckBox = document.querySelector("input[name='group-teenage']");
-  const kiokushinkaiCheckBox = document.querySelector(
-    "input[name='direction-kiokushinkai']",
-  );
-  const judoSamboCheckBox = document.querySelector(
-    "input[name='direction-judo_sambo']",
-  );
-  const setChecked = [
-    childCheckBox,
-    teenageCheckBox,
-    kiokushinkaiCheckBox,
-    judoSamboCheckBox,
-  ];
-  console.log(setChecked);
-  setChecked.forEach((checkBox) => {
-    if (checkBox) {
-      checkBox.setAttribute("checked", "checked");
-    }
-  });
+  if ((window.location.href = "subscribe.php")) {
+    console.log("Denis");
+    const childCheckBox = document.querySelector("input[name='group-child']");
+    const teenageCheckBox = document.querySelector(
+      "input[name='group-teenage']",
+    );
+    const kiokushinkaiCheckBox = document.querySelector(
+      "input[name='direction-kiokushinkai']",
+    );
+    const judoSamboCheckBox = document.querySelector(
+      "input[name='direction-judo_sambo']",
+    );
+    const setChecked = [
+      childCheckBox,
+      teenageCheckBox,
+      kiokushinkaiCheckBox,
+      judoSamboCheckBox,
+    ];
+    console.log(setChecked);
+    setChecked.forEach((checkBox) => {
+      if (checkBox) {
+        checkBox.setAttribute("checked", "checked");
+      }
+    });
+  }
 }
 function igor() {
   window.location.href = "subscribe.php";
-  console.log("Igor");
-  const adultCheckBox = document.querySelector("input[name='group-adult']");
-  const teenageCheckBox = document.querySelector("input[name='group-teenage']");
-  const judoSamboCheckBox = document.querySelector(
-    "input[name='direction-judo_sambo']",
-  );
-  const budokaiCheckBox = document.querySelector(
-    "input[name='direction-budokai']",
-  );
-  const setChecked = [
-    adultCheckBox,
-    teenageCheckBox,
-    budokaiCheckBox,
-    judoSamboCheckBox,
-  ];
-  console.log(setChecked);
-  setChecked.forEach((checkBox) => {
-    if (checkBox) {
-      checkBox.setAttribute("checked", "checked");
-    }
-  });
+  if ((window.location.href = "subscribe.php")) {
+    console.log("Igor");
+    const adultCheckBox = document.querySelector("input[name='group-adult']");
+    const teenageCheckBox = document.querySelector(
+      "input[name='group-teenage']",
+    );
+    const judoSamboCheckBox = document.querySelector(
+      "input[name='direction-judo_sambo']",
+    );
+    const budokaiCheckBox = document.querySelector(
+      "input[name='direction-budokai']",
+    );
+    const setChecked = [
+      adultCheckBox,
+      teenageCheckBox,
+      budokaiCheckBox,
+      judoSamboCheckBox,
+    ];
+    console.log(setChecked);
+    setChecked.forEach((checkBox) => {
+      if (checkBox) {
+        checkBox.setAttribute("checked", "checked");
+      }
+    });
+  }
 }
