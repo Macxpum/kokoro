fe91044c9d811401b24a4a1393f4171ddaa97ec6
Mashrum <mr_mashrum@mail.ru>
2026-05-05 18:56:39 +0300

    Add mobile CSS from provided screenshots and include it in head

--- include/head.php
+++ include/head.php
@@ -2,4 +2,5 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>KOKORO</title>
 <link rel="stylesheet" href="assets/styles/style.css">
+<link rel="stylesheet" href="assets/styles/figma_mobile.css">
 <link rel="icon" href="assets/images/kokoro_logo.webp" type="image/x-icon">
